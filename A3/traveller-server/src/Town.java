4cd688193d4b1ebc627cf9817fcd2bac6a5bb8c9
benjaminlau12 <lau.be@husky.neu.edu>
2021-02-03 18:33:29 -0500

    Added comments

--- A3/traveller-server/src/Town.java
+++ A3/traveller-server/src/Town.java
@@ -1,26 +1,42 @@
 import java.util.ArrayList;
 
-//
+/**
+ * Class that represents a Town that can be placed in a TownNetwork.
+ */
 public class Town {
   public ArrayList<Town> reachableTowns;
   public String townName;
 
+  /**
+   * Constructor of a Town that sets reachableTowns to an empty ArrayList and sets the given String
+   * as its townName
+   * @param tn
+   */
   public Town(String tn) {
     this.reachableTowns = new ArrayList<>();
     this.townName = tn;
 
   }
 
+  /**
+   * To add a path to the given Town
+   * @param t Town that will have a path to this Town
+   */
   public void addPath(Town t) {
+    //Checks that the given Town t is not the same as this Town
     if (!(this.townName.equals(t.townName))) {
+      //Avoids the existence of duplicate Towns in reachableTowns
       if (!(this.reachableTowns.contains(t))) {
         this.reachableTowns.add(t);
+        //Adds all reachableTowns from Town t to this.reachableTowns, while avoiding duplicates.
         for (Town town : t.reachableTowns) {
           if (!(this.reachableTowns.contains(town)) && !(this.equals(town))) {
             this.reachableTowns.add(town);
           }
         }
       }
+
+      //Adds all reachableTowns from this Town to t.reachableTowns, while avoiding duplicates.
       if (!(t.reachableTowns.contains(this))) {
         t.reachableTowns.add(this);
         for (Town town : this.reachableTowns) {
@@ -38,6 +54,11 @@ public class Town {
     }
   }
 
+  /**
+   * To determine if this Town can reach a given Town t.
+   * @param t Town to be reached
+   * @return boolean stating whether given Town t can be reached from this Town
+   */
   public boolean canReach(Town t) {
     return reachableTowns.contains(t) || this.townName.equals(t.townName);
   }
--- A3/traveller-server/src/TownNetwork.java
+++ A3/traveller-server/src/TownNetwork.java
@@ -3,30 +3,48 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-// Represents the entirety of the town network, maintaining towns, paths, and character positions.
+/**
+ * Class representing the entirety of the town network, maintaining towns, paths,
+ * and character positions.
+  */
+
 public class TownNetwork {
   public ArrayList<Town> towns;
   public ArrayList<Town[]> paths;
   public HashMap<Town, String> characterPositions;
 
+  /**
+   * Constructor of a TownNetwork that sets its paths and towns to an empty ArrayList and
+   * sets its character positions to a HashMap, in which the key is a Town and its value is the
+   * name of the Character at that Town.
+   */
   public TownNetwork() {
     this.towns = new ArrayList<Town>();
     this.paths = new ArrayList<Town[]>();
     this.characterPositions = new HashMap<Town, String>();
   }
 
-  // Adds the given town to this TownNetwork.  If the Town is already is part of the TownNetwork,
-  // it throws an IllegalArgumentException.
+  /**
+   * Adds the given town to this TownNetwork. If the Town is already is part of the TownNetwork,
+   * it throws an IllegalArgumentException.
+   * @param t Town to be added
+   */
   public void addTown(Town t) {
+    //Throws an IllegalArgumentException if Town t was already added.
     if (this.towns.contains(t)) {
       throw new IllegalArgumentException("Town is already in network.");
     }
     this.towns.add(t);
+    //Places Town t in HashMap with an empty String as its Character
     this.characterPositions.put(t, "");
   }
 
-  // Adds a path between the two given Towns.  If either of the towns are not already part of this
-  // TownNetwork or a path between them already exists, it throws an IllegalArgumentException.
+  /**
+   * Adds a path between the two given Towns. If either of the towns are not already part of this
+   * TownNetwork or a path between them already exists, it throws an IllegalArgumentException.
+   * @param t1 Town that is part of a new path to be added
+   * @param t2 Town that is part of a new path to be added
+   */
   public void addPath(Town t1, Town t2) {
     if (!this.towns.contains(t1) || !this.towns.contains(t2) || this.pathExists(t1, t2)) {
       throw new IllegalArgumentException("Unable to add path.");
@@ -34,16 +52,27 @@ public class TownNetwork {
     Town[] newPath = new Town[2];
     newPath[0] = t1;
     newPath[1] = t2;
+    //Adds new array of size two to paths, which represent the paths in this TownNetwork
     paths.add(newPath);
+    //Adds t2 to t1.reachableTowns
     t1.addPath(t2);
   }
 
-  // Places the given Character at the given Town.  If the given Town is already occupied by a
-  // Character, it throws an IllegalArgumentException.
+
+
+  /**
+   * Places the given Character at the given Town.  If the given Town is already occupied by a
+   * Character, it throws an IllegalArgumentException.
+   * @param c Character to be placed
+   * @param t Town in which given Character will be placed in
+   */
   public void placeCharacter(Character c, Town t) {
+    //Makes sure Town is not occupied by another Character
     if (characterPositions.get(t).equals("")) {
+      //Iterates through HashMap to check if Character c is in another Town
       for (Map.Entry<Town, String> entry : characterPositions.entrySet()) {
         if (entry.getValue().equals(c.getName())) {
+          //Replaces CharacterName with an empty String
           characterPositions.replace(entry.getKey(), "");
         }
       }
@@ -53,11 +82,19 @@ public class TownNetwork {
     }
   }
 
-  // Determines if a path exists that allows the given Character to travel to the given Town.
+
+
+  /**
+   * Determines if a path exists that allows the given Character to travel to the given Town.
+   * @param c Character that wants to reach given Town
+   * @param t Town to be reached
+   * @return boolean stating whether Character c can reach Town t
+   */
   public boolean canReach(Character c, Town t) {
     boolean hasTown = false;
     Town startTown = new Town("");
 
+    //Checks whether Character c is already in TownNetwork
     for (Map.Entry<Town, String> entry : characterPositions.entrySet()) {
       if (entry.getValue().equals(c.getName())) {
         hasTown = true;
@@ -71,8 +108,14 @@ public class TownNetwork {
     }
   }
 
-  // Private helper function used to determine if a path between two given Towns already exists
-  // in the TownNetwork.
+
+  /**
+   * Private helper function used to determine if a path between two given Towns already exists
+   * in the TownNetwork.
+   * @param t1 Town
+   * @param t2 Town
+   * @return boolean stating whether there already exists a path between t1 and t2
+   */
   private boolean pathExists(Town t1, Town t2) {
     if (this.paths.isEmpty()) {
       return false;
